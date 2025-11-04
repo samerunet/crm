e02ef0a3ca81c3d154515432a4f4492c8f163dff
samerunet <samerunet@gmail.com>
2025-11-03 23:51:45 -0800

    chore: guard stripe init

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,2 +1,7 @@
 import Stripe from 'stripe'
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2024-06-20' })
+
+const secretKey = process.env.STRIPE_SECRET_KEY
+
+export const stripe: Stripe | null = secretKey
+  ? new Stripe(secretKey, { apiVersion: '2024-06-20' })
+  : null
